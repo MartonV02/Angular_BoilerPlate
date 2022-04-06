a65563709693bbd8c320eb3a9ec42231153ed694
MartonVarga <69591271+MartonVarga@users.noreply.github.com>
2022-04-06 23:45:37 +0200

    NewAdd comp

--- AngularApp/src/app/app-routing.module.ts
+++ AngularApp/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home-component/home.component';
+import { NewAddComponent } from './components/new-add/new-add.component';
 import { TableSiteComponent } from './components/table-site/table-site.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path:'table-site', component: TableSiteComponent},
+  {path:'new-add', component: NewAddComponent},
 ];
 
 @NgModule({
